9617973348d27bb900e9302a00646389f5145085
Nak <58125177+R3mor@users.noreply.github.com>
2021-08-12 08:43:43 +0700

    resnet18 model update

--- featureExtraction/InceptionV3.py
+++ featureExtraction/InceptionV3.py
@@ -1,3 +1,8 @@
 import tensorflow as tf
 import keras
 
+tf.keras.applications.inception_v3.InceptionV3(
+    include_top=True, weights='imagenet', input_tensor=None,
+    input_shape=None, pooling=None, classes=1000,
+    classifier_activation='softmax'
+)
--- featureExtraction/ResNet18.py
+++ featureExtraction/ResNet18.py
@@ -1,6 +1,16 @@
 import torch
+from torchvision import *
+from torchvision.models import resnet
 
 resnet18_model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
 resnet18_model.eval()
 
 # For further steps: https://pytorch.org/hub/pytorch_vision_resnet/
+
+# The method above does not work properly for me, I researched further and found this instead, it works for me but just make sure
+# that you install the full pytorch package
+# use this: pip3 install torch==1.9.0+cu102 torchvision==0.10.0+cu102 torchaudio===0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+resnet18 = models.resnet18(pretrained=True)
+resnet18 = resnet18.cuda()
+print(resnet18)
