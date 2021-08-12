d0233e04721b164f00c1f5fc9a202be7f87a47c2
Wen Juin Pang <48470521+wjpang@users.noreply.github.com>
2021-08-12 18:36:50 +0800

    Added feature extraction class for InceptionV3

--- featureExtraction/InceptionV3.py
+++ featureExtraction/InceptionV3.py
@@ -1,3 +1,5 @@
+import torch
+import torch.nn as nn
 import torchvision.models as models
 
 # The tensorflow/keras version is not working, so it has been removed
@@ -7,3 +9,83 @@ import torchvision.models as models
 inception = models.inception_v3(pretrained=True)
 inception = inception.cpu()
 # print(inception)
+
+# Implementation below is adapted from https://discuss.pytorch.org/t/how-to-extract-features-of-an-image-from-a-trained-model/119
+# See post #49 by fmassa
+
+class MyInceptionFeatureExtractor(nn.Module):
+    def __init__(self, inception, transform_input=False):
+        super(MyInceptionFeatureExtractor, self).__init__()
+        self.transform_input = transform_input
+        self.Conv2d_1a_3x3 = inception.Conv2d_1a_3x3
+        self.Conv2d_2a_3x3 = inception.Conv2d_2a_3x3
+        self.Conv2d_2b_3x3 = inception.Conv2d_2b_3x3
+        self.maxpool1 = inception.maxpool1
+        self.Conv2d_3b_1x1 = inception.Conv2d_3b_1x1
+        self.Conv2d_4a_3x3 = inception.Conv2d_4a_3x3
+        self.maxpool2 = inception.maxpool2
+        self.Mixed_5b = inception.Mixed_5b
+        self.Mixed_5c = inception.Mixed_5c
+        self.Mixed_5d = inception.Mixed_5d
+        self.Mixed_6a = inception.Mixed_6a
+        self.Mixed_6b = inception.Mixed_6b
+        self.Mixed_6c = inception.Mixed_6c
+        self.Mixed_6d = inception.Mixed_6d
+        self.Mixed_6e = inception.Mixed_6e
+        self.Mixed_7a = inception.Mixed_7a
+        self.Mixed_7b = inception.Mixed_7b
+        self.Mixed_7c = inception.Mixed_7c
+        self.avgpool = inception.avgpool
+
+    def forward(self, x):
+        if self.transform_input:
+            x = x.clone()
+            x[0] = x[0] * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
+            x[1] = x[1] * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
+            x[2] = x[2] * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+        # 299 x 299 x 3
+        x = self.Conv2d_1a_3x3(x)
+        # 149 x 149 x 32
+        x = self.Conv2d_2a_3x3(x)
+        # 147 x 147 x 32
+        x = self.Conv2d_2b_3x3(x)
+        # 147 x 147 x 64
+        x = self.maxpool1(x)
+        # 73 x 73 x 64
+        x = self.Conv2d_3b_1x1(x)
+        # 73 x 73 x 80
+        x = self.Conv2d_4a_3x3(x)
+        # 71 x 71 x 192
+        x = self.maxpool2(x)
+        # 35 x 35 x 192
+        x = self.Mixed_5b(x)
+        # N x 256 x 35 x 35
+        x = self.Mixed_5c(x)
+        # N x 288 x 35 x 35
+        x = self.Mixed_5d(x)
+        # N x 288 x 35 x 35
+        x = self.Mixed_6a(x)
+        # N x 768 x 17 x 17
+        x = self.Mixed_6b(x)
+        # N x 768 x 17 x 17
+        x = self.Mixed_6c(x)
+        # N x 768 x 17 x 17
+        x = self.Mixed_6d(x)
+        # N x 768 x 17 x 17
+        x = self.Mixed_6e(x)
+        # N x 768 x 17 x 17
+        # N x 768 x 17 x 17
+        x = self.Mixed_7a(x)
+        # N x 1280 x 8 x 8
+        x = self.Mixed_7b(x)
+        # N x 2048 x 8 x 8
+        x = self.Mixed_7c(x)
+        # N x 2048 x 8 x 8
+        # Adaptive average pooling
+        x = self.avgpool(x)
+        return x
+
+"""Test"""
+new_inception = MyInceptionFeatureExtractor(inception)
+features = new_inception.forward(torch.randn([1, 3, 224, 224]))
+# print(features.shape)
