a5e818f5efca81020a342425bba6d8ee19e22bde
awad1tc <68623679+awad1tc@users.noreply.github.com>
2021-08-31 10:47:24 +1000

    Update svm_model.py

---
+++ classification_models/svm_model.py
@@ -0,0 +1,111 @@
+# Last edited by Alex Waddington 30/08/2021  - code partly based on methods written by
+# Avinash Navlani https://www.datacamp.com/community/tutorials/svm-classification-scikit-learn-python
+
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+
+
+# Import extratcted features
+IV3_features = pd.read_csv("extractedFeatures\\InceptionV3features.csv")
+Res_features = pd.read_csv("extractedFeatures\\ResNet18features.csv")
+VGG_features = pd.read_csv("extractedFeatures\\ResNet18features.csv")
+
+print(IV3_features.shape, Res_features.shape, VGG_features.shape)
+
+# Partition data into training and testing sets
+# For now only using IV3 - can expland later
+IV3_features = IV3_features.to_numpy()
+X_train_IV3, X_test_IV3, y_train_IV3, y_test_IV3 = train_test_split(
+    IV3_features[:, 0:9], IV3_features[:, 10]
+)
+
+# Perform gridsearch to set the hyperparatmeters C and gamma
+# Utility function to move the midpoint of a colormap to be around
+# the values of interest.
+class MidpointNormalize(Normalize):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
+
+
+# For an initial search, a logarithmic grid with basis
+# 10 is often helpful. Using a basis of 2, a finer
+# tuning can be achieved but at a much higher cost.
+C_range = np.logspace(-2, 10, 13)
+gamma_range = np.logspace(-9, 3, 13)
+param_grid = dict(gamma=gamma_range, C=C_range)
+cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+grid = GridSearchCV(svm.SVC(), param_grid=param_grid, cv=cv)
+grid.fit(X_train_IV3, y_train_IV3)
+
+print(
+    "The best parameters are %s with a score of %0.2f"
+    % (grid.best_params_, grid.best_score_)
+)
+
+# Now we need to fit a classifier for all parameters in the 2d version
+# (we use a smaller set of parameters here because it takes a while to train)
+
+C_2d_range = [1e-2, 1, 1e2]
+gamma_2d_range = [1e-1, 1, 1e1]
+classifiers = []
+for C in C_2d_range:
+    for gamma in gamma_2d_range:
+        clf = svm.SVC(C=C, gamma=gamma)
+        clf.fit(X_train_IV3, y_train_IV3)
+        classifiers.append((C, gamma, clf))
+
+# #############################################################################
+# Visualization
+#
+# draw visualization of parameter effects
+
+plt.figure(figsize=(8, 6))
+xx, yy = np.meshgrid(np.linspace(-3, 3, 200), np.linspace(-3, 3, 200))
+for (k, (C, gamma, clf)) in enumerate(classifiers):
+    # evaluate decision function in a grid
+    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    # visualize decision function for these parameters
+    plt.subplot(len(C_2d_range), len(gamma_2d_range), k + 1)
+    plt.title("gamma=10^%d, C=10^%d" % (np.log10(gamma), np.log10(C)), size="medium")
+
+scores = grid.cv_results_["mean_test_score"].reshape(len(C_range), len(gamma_range))
+
+# Draw heatmap of the validation accuracy as a function of gamma and C
+#
+# The score are encoded as colors with the hot colormap which varies from dark
+# red to bright yellow. As the most interesting scores are all located in the
+# 0.92 to 0.97 range we use a custom normalizer to set the mid-point to 0.92 so
+# as to make it easier to visualize the small variations of score values in the
+# interesting range while not brutally collapsing all the low score values to
+# the same color.
+
+plt.figure(figsize=(8, 6))
+plt.subplots_adjust(left=0.2, right=0.95, bottom=0.15, top=0.95)
+plt.imshow(
+    scores,
+    interpolation="nearest",
+    cmap=plt.cm.hot,
+    norm=MidpointNormalize(vmin=0.2, midpoint=0.92),
+)
+plt.xlabel("gamma")
+plt.ylabel("C")
+plt.colorbar()
+plt.xticks(np.arange(len(gamma_range)), gamma_range, rotation=45)
+plt.yticks(np.arange(len(C_range)), C_range)
+plt.title("Validation accuracy")
+plt.show()
+
+# Fit a modle as per the optimal hyperparameters
