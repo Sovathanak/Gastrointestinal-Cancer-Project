9ae8e41e027204bf724263a4f5fddcf3ed056429
vincenttaneli7 <82143257+vincenttaneli7@users.noreply.github.com>
2021-09-07 16:37:54 +0700

    resnet implementation (in progress)

---
+++ cnn_models/ResNet18_model.py
@@ -0,0 +1,64 @@
+import os
+import torch
+import torchvision
+import splitfolders
+from torch import nn
+from d2l import torch as d2l
+
+splitfolders.ratio("images2", output="split_output", seed=1337, ratio=(.6,.4))
+
+data_dir ="./split_output/"
+
+train_imgs = torchvision.datasets.ImageFolder(os.path.join(data_dir, 'train'))
+val_imgs = torchvision.datasets.ImageFolder(os.path.join(data_dir, 'val'))
+
+normalize = torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                             [0.229, 0.224, 0.225])
+
+train_augs = torchvision.transforms.Compose([
+    torchvision.transforms.RandomResizedCrop(224),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(), normalize])
+
+val_augs = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(256),
+    torchvision.transforms.CenterCrop(224),
+    torchvision.transforms.ToTensor(), normalize])
+
+pretrained_net = torchvision.models.resnet18(pretrained=True)
+print(pretrained_net.fc)
+
+finetune_net = torchvision.models.resnet18(pretrained=True)
+finetune_net.fc = nn.Linear(finetune_net.fc.in_features, 2)
+nn.init.xavier_uniform_(finetune_net.fc.weight);
+
+# If `param_group=True`, the model parameters in the output layer will be
+# updated using a learning rate ten times greater
+def train_fine_tuning(net, learning_rate, batch_size=2, num_epochs=5,
+                      param_group=True):
+    train_iter = torch.utils.data.DataLoader(
+        torchvision.datasets.ImageFolder(os.path.join(data_dir, 'train'),
+                                         transform=train_augs),
+        batch_size=batch_size, shuffle=True)
+    val_iter = torch.utils.data.DataLoader(
+        torchvision.datasets.ImageFolder(os.path.join(data_dir, 'val'),
+                                         transform=val_augs),
+        batch_size=batch_size)
+    devices = d2l.try_all_gpus()
+    loss = nn.CrossEntropyLoss(reduction="none")
+    if param_group:
+        params_1x = [
+            param for name, param in net.named_parameters()
+            if name not in ["fc.weight", "fc.bias"]]
+        trainer = torch.optim.SGD([{
+            'params': params_1x}, {
+                'params': net.fc.parameters(),
+                'lr': learning_rate * 10}], lr=learning_rate,
+                                  weight_decay=0.001)
+    else:
+        trainer = torch.optim.SGD(net.parameters(), lr=learning_rate,
+                                  weight_decay=0.001)
+    d2l.train_ch13(net, train_iter, val_iter, loss, trainer, num_epochs,
+                   devices)
+
+train_fine_tuning(finetune_net, 5e-5)
