ec8b9154ebb3ce94a0066cfd573667fded1bb506
awad1tc <68623679+awad1tc@users.noreply.github.com>
2021-09-14 18:35:34 +1000

    Neural net model

--- classification_models/decision_tree_model.py
+++ classification_models/decision_tree_model.py
@@ -7,14 +7,6 @@ import pandas as pd
 import numpy as np
 
 
-# Import the csv file
-IV3_features = pd.read_csv("extractedFeatures\\InceptionV3features.csv")
-Res_features = pd.read_csv("extractedFeatures\\ResNet18features.csv")
-VGG_features = pd.read_csv("extractedFeatures\\VGG16features.csv")
-
-# Randomly sample data ensuring proper target class distribution
-
-
 # Import extratcted features
 IV3_features = pd.read_csv("extractedFeatures\\InceptionV3features.csv")
 Res_features = pd.read_csv("extractedFeatures\\ResNet18features.csv")
